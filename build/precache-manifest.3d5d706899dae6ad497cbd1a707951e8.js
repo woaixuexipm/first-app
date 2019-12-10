6ab336dec3a82364234b261ca5bc8c04e8ff704c
woaixuexipm <w1157320590@163.com>
2019-12-10 14:44:56 +0800

    diyicibuild

---
+++ build/precache-manifest.3d5d706899dae6ad497cbd1a707951e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1b5d028551d2de197f2d96b990d3b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d18b27e3c5740e730b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d549a9287c2e51407a5c",
+    "url": "/static/js/2.010779ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.010779ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6d18b27e3c5740e730b",
+    "url": "/static/js/main.abf79629.chunk.js"
+  },
+  {
+    "revision": "d95d18e431b16709b21a",
+    "url": "/static/js/runtime-main.36828511.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
